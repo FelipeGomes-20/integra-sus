7991d3a80025bf7bf11736e829849e3944f6bd0c
Felipe Rosa Gomes <fgomes@tecnocomp.msft>
2025-07-21 02:31:57 -0300

    initial commit

---
+++ src/test/java/com/fiap/hackaton/integra_sus/IntegraSusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.hackaton.integra_sus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegraSusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
